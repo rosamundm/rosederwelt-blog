3bc7fa0c565cf32e983019dff639707377bfeb39
rosamund <rsmnd.mthr@gmail.com>
2020-02-09 19:53:24 +0000

    add files

--- mysite/blog/tests.py
+++
@@ -1,9 +0,0 @@
-from wagtail.tests.util import WagtailPageTests
-from blog.models import MyPage
-
-class MyPageTests(WagtailPageTests):
-    def
-
-from django.test import TestCase
-
-# Create your tests here.
