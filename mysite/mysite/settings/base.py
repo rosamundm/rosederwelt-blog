5dc7d2bdd14d41d28ec012c22c3dd39999969082
Rosamund <rsmnd.mthr@gmail.com>
2020-06-13 18:11:06 +0000

    updated todolist

--- mysite/mysite/settings/base.py
+++ mysite/mysite/settings/base.py
@@ -174,3 +174,5 @@ WAGTAIL_SITE_NAME = "mysite"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://rosederwelt.com'
+
+WAGTAIL_CODE_BLOCK_THEME = None
