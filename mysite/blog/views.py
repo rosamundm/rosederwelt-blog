02f3ab591d74673ed699d89762c0d13e2df7c0ff
Rosamund <rsmnd.mthr@gmail.com>
2021-10-18 14:39:08 +0000

    Fix HTTP handlers

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -11,3 +11,9 @@ def handler500(request, template_name):
     response = render("mynewwebsite/mysite/mysite/templates/500.html")
     response.status_code = 500
     return response
+
+
+def handler502(request, template_name):
+    response = render("mynewwebsite/mysite/mysite/templates/500.html")
+    response.status_code = 502
+    return response
